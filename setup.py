d09473c27e709928c42cb491c2b73d887c87a29d
Ad7amstein <adham32003200@gmail.com>
2025-08-02 11:24:01 +0300

    feat: Add setup script and requirements for the project

---
+++ setup.py
@@ -0,0 +1,26 @@
+"""Setup script for the group activity recognition package."""
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="group activity recognition",
+    version="1.0.0",
+    description="Hierarchical Deep Temporal Models for Group Activity Recognition.",
+    author="Ad7amstein",
+    author_email="adham32003200@gmail.com",
+    url="https://github.com/Ad7amstein/deep-activity-recognition",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    # install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+    entry_points={
+        "console_scripts": [
+        ],
+    },
+)
