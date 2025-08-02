844647d7e7fc4016a94c182c5dbde089714c2f60
Ad7amstein <adham32003200@gmail.com>
2025-08-02 12:31:26 +0300

    feat: enhance dataset download script to include unzipping functionality

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -3,8 +3,15 @@ echo -e "\nDownloading Dataset in Progress..."
 
 DATASET_LINK="https://www.kaggle.com/api/v1/datasets/download/ahmedmohamed365/volleyball"
 DEST_DIR="data/volleyball.zip"
+EX_DIR="data/volleyball"
+
+echo -e "\n✅Downloading Dataset Completed Successfully"
 
 curl -L --create-dirs -o "$DEST_DIR"\
   "$DATASET_LINK"
 
-echo -e "\n✅Downloading Dataset Completed Successfully"
+echo -e "\nUnzipping Files"
+
+unzip "$DEST_DIR" -d "$EX_DIR"
+
+echo -e "\n✅Done Unzipping Files"
