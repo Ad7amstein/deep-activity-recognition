4b2f636b443f156b8cfba22a70e4ad6267a11427
Adham 3llam <adham32003200@gmail.com>
2025-08-11 11:13:33 +0000

    feat: enhance FeatureExtractor to support tracking data and improve GPU memory management

--- src/data_processing/feature_extraction.py
+++ src/data_processing/feature_extraction.py
@@ -6,13 +6,18 @@ import torch
 from torch import nn
 from tqdm import tqdm
 from torchvision import transforms, models
-from pydantic_models.data_repr import VolleyballData
+from pydantic_models.data_repr import VolleyballData, TrackingData, ClipAnnotation
+from data_processing.annot_loading import AnnotationLoader
+from utils.config_utils import load_config
+
+CONFIG = load_config()
 
 
 class FeatureExtractor:
     def __init__(self, verbose: bool = False) -> None:
         self.model = None
         self.transform = None
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.verbose = verbose
 
     def prepare_model(
@@ -49,9 +54,8 @@ class FeatureExtractor:
                 ]
             )
 
-        device = "cuda" if torch.cuda.is_available() else "cpu"
         self.model = nn.Sequential(*(list(model.children())[:last_layer]))
-        self.model.to(device)
+        self.model.to(self.device)
         self.model.eval()
 
         return self.model, self.transform
@@ -67,42 +71,65 @@ class FeatureExtractor:
         verbose = self.verbose if not verbose else verbose
         if verbose:
             print("[INFO] Extracting Features from Volleyball Dataset...")
-        for video_id, video_annot_dct in tqdm(
-            volleyball_data.items(),
-            desc="Processing Videos",
-            disable=not verbose,
-            unit="video",
-        ):
-            video_path = os.path.join(videos_root, str(video_id))
-            for clip_id, clip_annot_dct in video_annot_dct.items():
-                clip_path = os.path.join(video_path, str(clip_id))
-                for frame_id, boxes_info in clip_annot_dct["tracking_annot_dct"][
-                    1
-                ].items():
-                    img_path = os.path.join(clip_path, f"{frame_id}.jpg")
-                    img_rgb = cv.cvtColor(cv.imread(img_path), cv.COLOR_BGR2RGB)
-                    preprocessed_imgs = []
+        with torch.inference_mode():
+            for video_id, video_annot_dct in tqdm(
+                volleyball_data.items(),
+                desc="Processing Videos",
+                disable=not verbose,
+                unit="video",
+            ):
+                video_path = os.path.join(videos_root, str(video_id))
+                for clip_id, clip_annot_dct in tqdm(
+                    video_annot_dct.items(),
+                    desc="Processing Clips",
+                    disable=not verbose,
+                    unit="clip",
+                ):
+                    clip_path = os.path.join(video_path, str(clip_id))
+                    for frame_id, boxes_info in clip_annot_dct["tracking_annot_dct"][
+                        1
+                    ].items():
+                        img_path = os.path.join(clip_path, f"{frame_id}.jpg")
+                        img_rgb = cv.cvtColor(cv.imread(img_path), cv.COLOR_BGR2RGB)
+                        preprocessed_imgs = []
 
-                    if img_level:
-                        preprocessed_imgs.append(self.transform(img_rgb))  # type: ignore
-                    else:
-                        for box_info in boxes_info:
-                            x1, y1, x2, y2 = box_info.box
-                            cropped_img = img_rgb[x1:x2, y1:y2]
-                            preprocessed_imgs.append(
-                                self.transform(cropped_img).unsqueeze(0)  # type: ignore
-                            )
-                    preprocessed_imgs = torch.cat(preprocessed_imgs)
-                    extracted_fetures = self.model(preprocessed_imgs)  # type: ignore
-                    np.save(output_file, extracted_fetures.numpy())
+                        if img_level:
+                            preprocessed_imgs.append(self.transform(img_rgb))  # type: ignore
+                        else:
+                            for box_info in boxes_info:
+                                x1, y1, x2, y2 = box_info.box
+                                cropped_img = img_rgb[y1:y2, x1:x2]
+                                preprocessed_imgs.append(
+                                    self.transform(cropped_img).unsqueeze(0)  # type: ignore
+                                )
+                        preprocessed_imgs = torch.cat(preprocessed_imgs)
+                        preprocessed_imgs = preprocessed_imgs.to(self.device)
+                        if img_level:
+                            preprocessed_imgs = preprocessed_imgs.unsqueeze(0)
+
+                        extracted_features = self.model(preprocessed_imgs)  # type: ignore
+                        extracted_features = extracted_features.view(
+                            extracted_features.shape[0], -1
+                        )
+                        np.save(output_file, extracted_features.cpu().detach().numpy())
+                        # Free GPU memory
+                        del preprocessed_imgs, extracted_features
+                        torch.cuda.empty_cache()
 
 
 def main():
     """Entry Point for the Program."""
     print(f"Welcome from `{os.path.basename(__file__).split('.')[0]}` Module.")
     feature_extractor = FeatureExtractor()
-    model, preprocessor = feature_extractor.prepare_model(
-        model=models.resnet50(pretrained=True)
+    _, _ = feature_extractor.prepare_model(model=models.resnet50(pretrained=True))
+    annot_loader = AnnotationLoader(verbose=True)
+    videos_root = os.path.join(CONFIG["PATH"]["data_root"], CONFIG["PATH"]["videos"])
+    feature_extractor.extract_features(
+        videos_root,
+        "out.npy",
+        annot_loader.load_pkl_version(verbose=True),
+        img_level=True,
+        verbose=True,
     )
 
 
