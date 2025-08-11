7bc83305f4030ec332e74c1d974e2ef2728bb8e3
Ad7amstein <adham32003200@gmail.com>
2025-08-11 10:03:57 +0300

    refactor: replace custom path validation methods with utility function

--- src/data_processing/annot_loading.py
+++ src/data_processing/annot_loading.py
@@ -10,6 +10,7 @@ import cv2 as cv
 from tqdm import tqdm
 from data_processing.box_info import BoxInfo
 from utils.config_utils import load_config
+from utils.path_utils import check_path
 from pydantic_models.data_repr import TrackingData, VolleyballData
 
 CONFIG = load_config()
@@ -142,40 +143,6 @@ class AnnotationLoader:
                 player_boxes=player_boxes_dct, frame_boxes=frame_boxes_dct
             )
 
-    @staticmethod
-    def check_dir(dir_path: str) -> None:
-        """
-        Checks if the provided path is a valid directory.
-        Args:
-            dir_path (str): The path to the directory to check.
-        Raises:
-            TypeError: If `dir_path` is not a string.
-            ValueError: If `dir_path` does not exist or is not a directory.
-        """
-
-        if not isinstance(dir_path, str):
-            raise TypeError(
-                f"directory must be a string, got {type(dir_path).__name__}"
-            )
-        if not os.path.isdir(dir_path):
-            raise ValueError(f"{dir_path} does not exist or is not a directory.")
-
-    @staticmethod
-    def check_file(file_path: str) -> None:
-        """
-        Validates that the provided file_path path is a string and points to an existing file.
-        Args:
-            file_path (str): Path to the file.
-        Raises:
-            TypeError: If `file_path` is not a string.
-            ValueError: If `file_path` does not exist or is not a file.
-        """
-
-        if not isinstance(file_path, str):
-            raise TypeError(f"file must be a string, got {type(file_path).__name__}")
-        if not os.path.isfile(file_path):
-            raise ValueError(f"{file_path} does not exist or is not a file.")
-
     def load_video_annot(
         self,
         video_annot_file: str,
@@ -201,8 +168,8 @@ class AnnotationLoader:
         if verbose:
             print(f"[INFO] Loading Video Annotations from {video_annot_file}")
         clip_category_dct = {}
-        AnnotationLoader.check_file(video_annot_file)
-        AnnotationLoader.check_dir(tracking_annot_root)
+        check_path(video_annot_file, "file")
+        check_path(tracking_annot_root, "dir")
 
         with open(video_annot_file, mode="r", encoding="utf-8") as file:
             lines = file.readlines()
@@ -252,8 +219,8 @@ class AnnotationLoader:
         verbose = self.verbose if verbose is None else verbose
         if verbose:
             print(f"[INFO] Loading Volleyball Dataset From {videos_root}...")
-        AnnotationLoader.check_dir(videos_root)
-        AnnotationLoader.check_dir(tracking_annot_root)
+        check_path(videos_root, "dir")
+        check_path(tracking_annot_root, "dir")
         video_annot_dct = {}
         for video_dir in tqdm(
             os.listdir(videos_root),
@@ -337,7 +304,7 @@ class AnnotationLoader:
 
         verbose = self.verbose if verbose is None else verbose
         data_path = os.path.join(CONFIG["PATH"]["data_root"], "volleyball_dataset.pkl")
-        AnnotationLoader.check_file(data_path)
+        check_path(data_path, "file")
         if verbose:
             print(f"[INFO] Loading Data from pickle file: {data_path}")
 
