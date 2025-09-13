0eae633c6d7cb1a0e1ad40ba16aff9455b4de78d
Ad7amstein <adham32003200@gmail.com>
2025-09-13 19:47:06 +0300

    feat: implement BaseController with app settings initialization and main entry point

---
+++ src/controllers/base_controller.py
@@ -0,0 +1,19 @@
+import os
+from utils.config_utils import get_settings
+from torch.utils.data import Dataset
+
+
+class BaseController:
+    def __init__(self) -> None:
+        self.app_settings = get_settings()
+
+
+def main():
+    """Entry Point for the Program."""
+    print(
+        f"Welcome from `{os.path.basename(__file__).split('.')[0]}` Module. Nothing to do ^_____^!"
+    )
+
+
+if __name__ == "__main__":
+    main()
