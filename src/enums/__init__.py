56386abe54e990db204c802ce7bbdeb4b99790aa
Ad7amstein <adham32003200@gmail.com>
2025-09-13 10:20:11 +0300

    feat: add activity category to label mappings in enums

---
+++ src/enums/__init__.py
@@ -0,0 +1,23 @@
+from enums.activity import ActivityEnum
+
+activity_category2label_dct = {
+    ActivityEnum.RIGHT_SET_CATEGORY.value: ActivityEnum.RIGHT_SET_LABEL.value,
+    ActivityEnum.RIGHT_PASS_CATEGORY.value: ActivityEnum.RIGHT_PASS_LABEL.value,
+    ActivityEnum.RIGHT_WINPOINT_CATEGORY.value: ActivityEnum.RIGHT_WINPOINT_LABEL.value,
+    ActivityEnum.RIGHT_SPIKE_CATEGORY.value: ActivityEnum.RIGHT_SPIKE_LABEL.value,
+    ActivityEnum.LEFT_SET_CATEGORY.value: ActivityEnum.LEFT_SET_LABEL.value,
+    ActivityEnum.LEFT_PASS_CATEGORY.value: ActivityEnum.LEFT_PASS_LABEL.value,
+    ActivityEnum.LEFT_WINPOINT_CATEGORY.value: ActivityEnum.LEFT_WINPOINT_LABEL.value,
+    ActivityEnum.LEFT_SPIKE_CATEGORY.value: ActivityEnum.LEFT_SPIKE_LABEL.value,
+}
+
+activity_label2category_dct = {
+    ActivityEnum.RIGHT_SET_LABEL.value: ActivityEnum.RIGHT_SET_CATEGORY.value,
+    ActivityEnum.RIGHT_PASS_LABEL.value: ActivityEnum.RIGHT_PASS_CATEGORY.value,
+    ActivityEnum.RIGHT_WINPOINT_LABEL.value: ActivityEnum.RIGHT_WINPOINT_CATEGORY.value,
+    ActivityEnum.RIGHT_SPIKE_LABEL.value: ActivityEnum.RIGHT_SPIKE_CATEGORY.value,
+    ActivityEnum.LEFT_SET_LABEL.value: ActivityEnum.LEFT_SET_CATEGORY.value,
+    ActivityEnum.LEFT_PASS_LABEL.value: ActivityEnum.LEFT_PASS_CATEGORY.value,
+    ActivityEnum.LEFT_WINPOINT_LABEL.value: ActivityEnum.LEFT_WINPOINT_CATEGORY.value,
+    ActivityEnum.LEFT_SPIKE_LABEL.value: ActivityEnum.LEFT_SPIKE_CATEGORY.value,
+}
