ecaabb5156f6e5398850629aad91b9d02e2e35a9
Adham 3llam <adham32003200@gmail.com>
2025-09-13 21:01:06 +0000

    fix: update some imports and logging

--- src/controllers/train_controller.py
+++ src/controllers/train_controller.py
@@ -5,8 +5,9 @@ from torch import nn
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from controllers.base_controller import BaseController
-from data_processing import AnnotationLoader
+from data_processing.annot_loading import AnnotationLoader
 from utils.model_utils import train
+from utils.stream_utils import log_stream
 from enums.model import ModelMode
 from enums.baselines import B1Enum
 from stores.baselines.providers import B1CustomDataset, B1Model
@@ -16,7 +17,9 @@ class TrainController(BaseController):
     def __init__(self, DatasetClass, model: nn.Module) -> None:
         super().__init__()
         self.model = model
-        self.volleyball_data = AnnotationLoader(verbose=True).load_pkl_version()
+        self.volleyball_data = AnnotationLoader(verbose=True).load_pkl_version(
+            verbose=True
+        )
         self.DatasetClass = DatasetClass
         self.loss_fn = nn.CrossEntropyLoss()
         self.optimizer = torch.optim.SGD(params=model.parameters(), lr=B1Enum.LR.value)
@@ -47,8 +50,10 @@ class TrainController(BaseController):
 
 def main():
     """Entry Point for the Program."""
+    log_stream(log_file="b1", prog="train", verbose=True, overwrite=True)
     print(f"Welcome from `{os.path.basename(__file__).split('.')[0]}` Module.")
     train_controller = TrainController(B1CustomDataset, B1Model())
+    train_controller.train()
 
 
 if __name__ == "__main__":
