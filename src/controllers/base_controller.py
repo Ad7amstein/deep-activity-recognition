588c3b75f0c710ec4b9dfa69b8845d18941aed56
Ad7amstein <adham32003200@gmail.com>
2025-09-28 09:09:15 +0300

    feat: initialize BaseController with seed setting and logging capabilities

--- src/controllers/base_controller.py
+++ src/controllers/base_controller.py
@@ -1,11 +1,37 @@
 import os
+from typing import Optional
+import random
+import numpy as np
+import torch
 from utils.config_utils import get_settings
-from torch.utils.data import Dataset
+from utils.logging_utils import setup_logger
 
 
 class BaseController:
-    def __init__(self) -> None:
+    def __init__(
+        self, seed_value: int = 42, set_seeds: bool = True, verbose: bool = True
+    ) -> None:
+        self.verbose = verbose
         self.app_settings = get_settings()
+        self.logger = setup_logger(
+            logger_name=__name__,
+            log_file=__file__,
+            log_dir=self.app_settings.PATH_LOGS,
+            log_to_console=self.verbose,
+        )
+        if set_seeds:
+            self.set_all_seeds(seed_value=seed_value)
+
+    def set_all_seeds(self, seed_value: int, verbose: Optional[bool] = None) -> None:
+        self.logger.info("Setting all seeds...")
+        verbose = verbose if verbose is not None else self.verbose
+        torch.manual_seed(seed_value)
+        torch.cuda.manual_seed_all(seed_value)
+        np.random.seed(seed_value)
+        random.seed(seed_value)
+        os.environ["PYTHONHASHSEED"] = str(seed_value)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
 
 
 def main():
@@ -13,6 +39,7 @@ def main():
     print(
         f"Welcome from `{os.path.basename(__file__).split('.')[0]}` Module. Nothing to do ^_____^!"
     )
+    BaseController()
 
 
 if __name__ == "__main__":
