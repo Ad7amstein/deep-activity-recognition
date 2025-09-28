6515fe14e158037f37253c65b8f49e90402b47e6
Ad7amstein <adham32003200@gmail.com>
2025-09-28 22:39:31 +0300

    feat: add InferenceRequest model and main function in ai.py for AI processing

---
+++ src/routes/schemas/ai.py
@@ -0,0 +1,17 @@
+import os
+from pydantic import BaseModel
+
+
+class InferenceRequest(BaseModel):
+    baseline_number: int
+
+
+def main():
+    """Entry point for the program (for standalone execution)."""
+    print(
+        f"Welcome from `{os.path.basename(__file__).split('.')[0]}` Module. Nothing to do ^_____^!"
+    )
+
+
+if __name__ == "__main__":
+    main()
