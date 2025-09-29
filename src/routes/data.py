e74b37100ee81561919fb7f81f57d9b494067baf
Ad7amstein <adham32003200@gmail.com>
2025-09-29 14:35:13 +0300

    feat: implement data upload endpoint with file validation and logging

---
+++ src/routes/data.py
@@ -0,0 +1,70 @@
+import os
+import aiofiles
+from fastapi import APIRouter, UploadFile, status, Request
+from fastapi.responses import JSONResponse
+from controllers import DataController
+from utils.logging_utils import setup_logger
+from models.enums import ResponseSignalEnum
+
+
+data_router = APIRouter(prefix="/api/v1/data", tags=["api_v1", "data"])
+
+
+@data_router.post("/upload/{project_id}")
+async def upload_data(
+    request: Request,
+    project_id: int,
+    file: UploadFile,
+):
+    app_settings = request.app.state.settings
+    logger = setup_logger(
+        logger_name=__name__,
+        log_file=__file__,
+        log_dir=app_settings.PATH_LOGS,
+        log_to_console=True,
+        use_tqdm=True,
+        file_mode="a",
+    )
+    data_controller = DataController()
+    is_valid, result_signal = data_controller.validate_uploaded_file(file)
+
+    if not is_valid:
+        return JSONResponse(
+            content={"signal": result_signal}, status_code=status.HTTP_400_BAD_REQUEST
+        )
+
+    file_path, file_name = data_controller.generate_unique_filepath(
+        original_file_name=file.filename, project_id=project_id  # type: ignore
+    )
+
+    logger.info("File Upload in progress...")
+    try:
+        async with aiofiles.open(file_path, mode="wb") as f:  # type: ignore
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+    except OSError as exc:
+        logger.error("Error while uploading file: %s", exc)
+        return JSONResponse(
+            content={"signal": ResponseSignalEnum.FILE_UPLOAD_FAILED.value},
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+    return JSONResponse(
+        content={
+            "signal": ResponseSignalEnum.FILE_UPLOAD_SUCCESS.value,
+            "file_name": file_name,
+            "file_path": file_path,
+        },
+        status_code=status.HTTP_200_OK,
+    )
+
+
+def main():
+    """Entry Point for the Program."""
+    print(
+        f"Welcome from `{os.path.basename(__file__).split('.')[0]}` Module. Nothing to do ^_____^!"
+    )
+
+
+if __name__ == "__main__":
+    main()
